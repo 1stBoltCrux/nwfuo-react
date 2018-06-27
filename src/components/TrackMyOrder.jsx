b887b196790b72c8259ba0d42f478c26c0190122
Matt Dowdney and Ted Leary <matt+ted@>
2018-06-27 14:55:58 -0700

    add image loader

--- src/components/TrackMyOrder.jsx
+++ src/components/TrackMyOrder.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import canyun from '../assets/images/grandcanyon.jpg';
 
 function TrackMyOrder(){
   return(
     <div className='track-my-order-wrapper'>
       <p>We are not currently tracking orders.</p>
+       <img src={canyun}/>
     </div>
   );
 }
